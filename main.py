505fd8dc67ac3bea14d30a03472fd2ce9074dec5
rafalwinnik0 <130064427+rafalwinnik0@users.noreply.github.com>
2023-04-28 13:54:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,21 +8,31 @@ data = pandas.read_csv("french_words.csv")
 new_dict = data.to_dict(orient="records")
 new_random = {}
 
+def right_answer():
+    new_dict.remove(new_random)
+    data = pandas.DataFrame(new_dict)
+    data.to_csv("words_to_learn.csv")
+    random_word()
+
 def random_word():
-    global new_random
+    global new_random, flip_timer
+    window.after_cancel(flip_timer)
     new_random = random.choice(new_dict)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=new_random["French"])
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=new_random["French"], fill="black")
+    canvas.itemconfig(canvas_image, image=image_front)
+    flip_timer = window.after(3000, func=flip_card)
 
 def flip_card():
-    canvas.itemconfig(card_title, text="English")
-    canvas.itemconfig(card_word, text=new_random["English"])
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=new_random["English"], fill="white")
+    canvas.itemconfig(canvas_image, image=image_back)
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-window.after(3000, func=flip_card)
+flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(window, height=526, width=800, bg=BACKGROUND_COLOR, highlightthickness=0)
 image_front = PhotoImage(file="card_front.png")
@@ -38,11 +48,9 @@ button_left = Button(image=image_left, highlightthickness=0, command=random_word
 button_left.grid(row=1, column=0)
 
 image_right = PhotoImage(file="right.png")
-button_right = Button(image=image_right, highlightthickness=0, command=random_word)
+button_right = Button(image=image_right, highlightthickness=0, command=right_answer)
 button_right.grid(row=1, column=1)
 
 random_word()
-
-
 
 window.mainloop()
